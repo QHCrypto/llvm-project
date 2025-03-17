68ab746a4af1ec6d4a72bd712c1444edfa9efc10
Guyutongxue <guyutongxue@163.com>
2025-03-17 10:47:34 +0800

    use a more internal method to implement [[attr]] AST

--- clang/lib/Sema/ParsedAttr.cpp
+++ clang/lib/Sema/ParsedAttr.cpp
@@ -10,7 +10,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "clang/Sema/BoncParsedAttr.hpp"
 #include "clang/Sema/ParsedAttr.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/Basic/AttrSubjectMatchRules.h"
@@ -28,8 +27,6 @@
 using namespace clang;
 
 LLVM_INSTANTIATE_REGISTRY(ParsedAttrInfoRegistry)
-
-static ParsedAttrInfoRegistry::Add<Bonc::ExampleAttrInfo> X("example", "");
 
 IdentifierLoc *IdentifierLoc::create(ASTContext &Ctx, SourceLocation Loc,
                                      IdentifierInfo *Ident) {
--- clang/lib/Sema/SemaDeclAttr.cpp
+++ clang/lib/Sema/SemaDeclAttr.cpp
@@ -8438,6 +8438,9 @@ static void ProcessDeclAttribute(Sema &S, Scope *scope, Decl *D,
   case ParsedAttr::AT_UsingIfExists:
     handleSimpleAttribute<UsingIfExistsAttr>(S, D, AL);
     break;
+  case ParsedAttr::AT_BoncRound:
+    handleSimpleAttribute<BoncRoundAttr>(S, D, AL);
+    break;
   }
 }
 
